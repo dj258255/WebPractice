5662f0b5e8d67423c67a97827085666e43bd94ea
dj258255 <dj258255@naver.com>
2025-01-30 21:07:28 +0900

    TodoDAO VO
    VO 테스트 (등록기능)

--- jdbcex/src/test/java/org/zerock/dao/ConnectTests.java
+++ jdbcex/src/test/java/org/zerock/dao/ConnectTests.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Test;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 public class ConnectTests {
 
     @Test
@@ -18,6 +21,25 @@ public class ConnectTests {
                 "webuser",
                 "webuser");
         Assertions.assertNotNull(connection);
+
+        connection.close();
+    }
+
+    @Test
+    public void testHikariCP() throws Exception{
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName("org.mariadb.jdbc.Driver");
+        config.setJdbcUrl("jdbc:mariadb://localhost:3306/webdb");
+        config.setUsername("webuser");
+        config.setPassword("webuser");
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+
+        HikariDataSource ds = new HikariDataSource(config);
+        Connection connection = ds.getConnection();
+
+        System.out.println(connection);
 
         connection.close();
     }
