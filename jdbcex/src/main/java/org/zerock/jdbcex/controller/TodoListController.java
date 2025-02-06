fee014846cb12ebf1df953fd7c119df5f5481557
dj258255 <dj258255@naver.com>
2025-02-06 22:44:54 +0900

    목록 기능 구현
    ㅇ

--- jdbcex/src/main/java/org/zerock/jdbcex/controller/TodoListController.java
+++ jdbcex/src/main/java/org/zerock/jdbcex/controller/TodoListController.java
@@ -1,6 +1,7 @@
 package org.zerock.jdbcex.controller;
 
 import lombok.extern.log4j.Log4j2;
+import org.zerock.jdbcex.dto.TodoDTO;
 import org.zerock.jdbcex.service.TodoService;
 
 import javax.servlet.ServletException;
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "todoListController", value = "/todo/list")
 @Log4j2
@@ -19,6 +21,15 @@ public class TodoListController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         log.info("todo list..............");
+
+        try{
+            List<TodoDTO> dtoList = todoService.listAll();
+            req.setAttribute("dtoList",dtoList);
+            req.getRequestDispatcher("/WEB-INF/todo/list.jsp").forward(req,resp);
+        } catch ( Exception e){
+            log.error(e.getMessage());
+            throw new ServletException(e);
+        }
     }
 }
 
