4912f256a0b35d304a26a052f67f5c6e14ca45f9
dj258255 <dj258255@naver.com>
2025-04-16 07:55:27 +0900

    리스트 html
    d

--- roomtrip/src/main/java/org/roomtraveler/roomtrip/controller/BoardController.java
+++ roomtrip/src/main/java/org/roomtraveler/roomtrip/controller/BoardController.java
@@ -2,7 +2,9 @@ package org.roomtraveler.roomtrip.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.roomtraveler.roomtrip.dto.BoardDTO;
 import org.roomtraveler.roomtrip.dto.PageRequestDTO;
+import org.roomtraveler.roomtrip.dto.PageResponseDTO;
 import org.roomtraveler.roomtrip.service.BoardService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,7 +20,13 @@ public class BoardController {
     private final BoardService boardService;
 
     @GetMapping("/list")
-    public void list(PageRequestDTO pageRequestDTO, Model mode){
+    public void list(PageRequestDTO pageRequestDTO, Model model){
+
+        PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
+
+        log.info(responseDTO);
+
+        model.addAttribute("responseDTO", responseDTO);
 
     }
 }
